3ea431209a2dd0b241c5389ea64c3eec2641fa18
Tommyixi <carpenito.t@gmail.com>
2021-01-05 18:39:12 -0500

    begins functionality to accept MISL

---
+++ R/mice.impute.misl.R
@@ -0,0 +1,13 @@
+#' Imputation by MISL
+#'
+#'
+#' @inheritParams mice.impute.pmm
+#' @return Vector with imputed data, same type as \code{y}, and of length
+#'
+#'
+#' @family univariate imputation functions
+#' @keywords datagen
+#' @export
+mice.impute.misl <- function(y, ry, x = NULL, wy = NULL, ...) {
+
+}
---
+++ tests/testthat/test-mice.impute.misl.R
@@ -0,0 +1,2 @@
+context("mice.impute.misl")
+
